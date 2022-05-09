96e0a4dd27cd9ffd64c344f457452c1e3ef1fb64
Shrenika, Soma <shrenika_soma@optum.com>
2022-05-09 14:28:41 +0530

    Initial Commit

---
+++ Toys ExchangeUITests/Toys_ExchangeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Toys_ExchangeUITestsLaunchTests.swift
+//  Toys ExchangeUITests
+//
+//  Created by Shrenika, Soma on 09/05/22.
+//
+
+import XCTest
+
+class Toys_ExchangeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
